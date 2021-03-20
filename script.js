79e100ccf68a81de6bb24e2d58adac2403957ba5
Chance Robinson <fpllblya@gmail.com>
2021-03-20 11:48:59 -0500

    add card pages and IDs

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 M.AutoInit();
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+    var instances = M.Carousel.init(elems,
+        { 
+        fullWidth: false,
+        indicators: false,
+        // duration: 1000
+        }
+    );
 });
-var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true,
-    duration: 100
-});
+// var instance = M.Carousel.init({
+//     fullWidth: true,
+//     indicators: true,
+//     duration: 1000
+// });
