7cc1ff106f2f3c756077bd209c76950d1f6c1cd2
Jason Sharpless <strawhatsociety@yahoo.com>
2021-03-20 10:53:31 -0700

    added js functions

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ document.addEventListener('DOMContentLoaded', function() {
 //     indicators: true,
 //     duration: 1000
 // });
+var carousel = document.getElementById("top-carousel")
+var moviePosters = document.getElementById("movie-posters-grid")
+var cardPage = document.getElementById("card-page")
+
+function startpage(){
+carousel.style.display="block";
+moviePosters.style.display="block";
+cardPage.style.display="none";
+}
+startpage()
+
+moviePosters.onclick=function(){
+    carousel.style.display="none";
+moviePosters.style.display="none";
+cardPage.style.display="block";
+}
