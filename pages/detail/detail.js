515c6e06f3585c2ca9fcc9a4f286d3ecb7e8fd4b
Belle Duran <bduran04@gmail.com>
2021-03-22 22:22:22 -0500

    applied the youtube api on the html

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,7 +1,10 @@
 var omdbAppId = 'trilogy';
+var youtubeAPI = 'AIzaSyDy0Z1hGuIAeJbvhnVXeGE-3W7roHgRQis';
 var omdbPath = 'http://www.omdbapi.com/';
+var youtubePath = 'https://youtube.googleapis.com/youtube/v3/search';
 
 var movieDetails = {};
+var movieTrailer = {};
 
 var urlParams = new URLSearchParams(window.location.search);
 var movie = urlParams.get('movie');
@@ -15,13 +18,19 @@ async function retrieveMovieInfo(movie) {
     await fetch(`${omdbPath}?t=${movie}&apikey=${omdbAppId}`)
         .then(function(response) { return response.json() }) // Convert data to json
         .then(function(data) {movieDetails = data;})
-}
+    await fetch(`${youtubePath}?part=snippet&part=id&channelId=UCi8e0iOVk1fEOogdfu4YgfA&q=${movie}&key=${youtubeAPI}`)
+    .then(function(response) { return response.json() })
+    .then(function(data) {movieTrailer = data;})
+    console.log(movieTrailer)
+    }
 
 function buildHTMLAndAssignValues() {
     $(".brand-logo").text(movieDetails.Title);
     $(".card-movie-title").text(movieDetails.Title);
     $(".card-movie-plot").text(`Movie Plot: ${movieDetails.Plot}`)
     $(".card-movie-rating").text(`Rotten Tomatoes Rating: ${movieDetails.Ratings[1].Value}`)
+    $(".video-container iframe").attr("src", `https://www.youtube.com/embed/${movieTrailer.items[0].id.videoId}`);
 
+//if no rotten tomato rating is available, state that no rotten tomato rating is available 
 }
 init()
