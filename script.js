8f4d599ed425c46990f9805ad40ac5755d10e9d2
Belle Duran <bduran04@gmail.com>
2021-03-22 19:48:33 -0500

    updated the code to pull data from omdb

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 M.AutoInit();
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems,
-        { 
-        fullWidth: false,
-        indicators: false,
-        // duration: 1000
-        }
-    );
-});
-// var instance = M.Carousel.init({
-//     fullWidth: true,
-//     indicators: true,
-//     duration: 1000
-// });
+
 var carousel = document.getElementById("top-carousel")
 var moviePosters = document.getElementById("movie-posters-grid")
 var cardPage = document.getElementById("card-page")
@@ -25,8 +11,3 @@ cardPage.style.display="none";
 }
 startpage()
 
-moviePosters.onclick=function(){
-    carousel.style.display="none";
-moviePosters.style.display="none";
-cardPage.style.display="block";
-}
