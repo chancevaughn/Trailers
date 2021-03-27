b1d3eb564d11feebfeb9a89040766697e4d7f186
Chance Robinson <fpllblya@gmail.com>
2021-03-27 01:21:44 -0500

    final changes and added favicon

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 M.AutoInit();
 
-var carousel = document.getElementById("top-carousel")
-var moviePosters = document.getElementById("movie-posters-grid")
-var cardPage = document.getElementById("card-page")
-var allMovies = document.getElementById("all-movies")
-var navbar = document.getElementById("navbar")
+// var carousel = document.getElementById("top-carousel")
+// var moviePosters = document.getElementById("movie-posters-grid")
+// var cardPage = document.getElementById("card-page")
+// var allMovies = document.getElementById("all-movies")
+// var navbar = document.getElementById("navbar")
 
-function startpage(){
-carousel.style.display="block";
-moviePosters.style.display="block";
-cardPage.style.display="none";
-navbar.style.display="block"
-}
-startpage()
+// function startpage(){
+// carousel.style.display="block";
+// moviePosters.style.display="block";
+// cardPage.style.display="none";
+// navbar.style.display="block"
+// }
+// startpage()
 
-moviePosters.onclick = function(){
-    carousel.style.display="none";
-    moviePosters.style.display="none";
-    cardPage.style.display="block";
-    navbar.style.display="none"
-}
+// moviePosters.onclick = function(){
+//     carousel.style.display="none";
+//     moviePosters.style.display="none";
+//     cardPage.style.display="block";
+//     navbar.style.display="none"
+// }
 
-allMovies.onclick = function(){
-    carousel.style.display="block";
-    moviePosters.style.display="block";
-    cardPage.style.display="none";
-    navbar.style.display="block"
-}
-$(".dropdown-trigger").dropdown();
+// allMovies.onclick = function(){
+//     carousel.style.display="block";
+//     moviePosters.style.display="block";
+//     cardPage.style.display="none";
+//     navbar.style.display="block"
+// }
+// $(".dropdown-trigger").dropdown();
