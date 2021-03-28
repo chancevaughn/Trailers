ccd734521f4b0055a7fd08dc4297135b4ed4de2f
Belle Duran <bduran04@gmail.com>
2021-03-27 19:32:58 -0500

    updated the movie titles so they may be easier to be appended in the fav page

--- favorites.js
+++ favorites.js
@@ -1,9 +1,8 @@
 var favoriteMovies = JSON.parse((window.localStorage.getItem('movie')));
 //loop through the array to grab the keys of each index
 for (var i = 0; i < favoriteMovies.length; i++) {
-$("#movie-posters-grid").append(`
+$(".movies-container").append(`
 <div class="col s4 l3">
-<div class="row"></div>
 <div class="card responsive-img">
 <a href="${favoriteMovies[i].href}" class="card-image">
   <img src="Images/movie-posters/${favoriteMovies[i].src}.jpeg">
