4ae66797386255b684e613da84c56c61f2a71fff
Belle Duran <bduran04@gmail.com>
2021-03-27 20:01:29 -0500

    updated the favorite.js in order to better format the movie items

--- favorites.js
+++ favorites.js
@@ -1,7 +1,7 @@
 var favoriteMovies = JSON.parse((window.localStorage.getItem('movie')));
 //loop through the array to grab the keys of each index
 for (var i = 0; i < favoriteMovies.length; i++) {
-$(".movies-container").append(`
+    $(".movies-container").append(`
 <div class="col s4 l3">
 <div class="card responsive-img">
 <a href="${favoriteMovies[i].href}" class="card-image">
